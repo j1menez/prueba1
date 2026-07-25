696159911c22b2224ba895e2cf9714f14e443c5e
j1menez <carloosjiimenez@gmail.com>
2026-07-25 11:39:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 function bienvenida(){
     alert("bienvenio al mundo del software ponto tendras money ya veras!! el universo ya lo autorizo la vida y el destino y dios te dara la salus y sabiduria cuida y ama a tu mai");
 }
+
+function ferrari(){
+    alert("lo conseguiras cabron lo se estoy seguro q asi sera <3 <3 <3 amennnnnnnnn");
+}

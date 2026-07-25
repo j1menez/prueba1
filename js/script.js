f99091e943a12c18b126d5157b9ac2337e2632ce
jimenez <carloosjiimenez@gmail.com>
2026-07-25 09:58:03 -0500

    avance fase 1

---
+++ js/script.js
@@ -0,0 +1,3 @@
+function bienvenida(){
+    alert("bienvenio al mundo del software ponto tendras money ya veras!! el universo ya lo autorizo la vida y el destino y dios te dara la salus y sabiduria cuida y ama a tu mai");
+}
